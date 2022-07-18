ae0b37450d57fd113ff1784c9f82460eb920a7c3
Gilberto Lupatini <gilberto.lupatini@gmail.com>
2022-07-18 19:56:38 -0300

    Thymeleaf

--- src/main/java/br/com/tt/petfeliz/controller/AdministracaoWebController.java
+++ src/main/java/br/com/tt/petfeliz/controller/AdministracaoWebController.java
@@ -1,14 +1,15 @@
 package br.com.tt.petfeliz.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller //Não com REST, mas com Páginas HTML
 public class AdministracaoWebController {
 
-    @GetMapping("/admin")
-    public String getPaginaInicial(){
-        //page.addAttribute("nomeAplicacao", "Pet Feliz");
+    @GetMapping({"/admin/", "/admin"})
+    public String getPaginaInicial(Model page){
+        page.addAttribute("nomeAplicacao", "Pet Feliz");
         return "admin"; // admin.html
     }
 }
