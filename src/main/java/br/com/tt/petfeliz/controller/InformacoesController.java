7bb367896d99578353e4ef525c37b8ed03f6e3fe
Gilberto Lupatini <gilberto.lupatini@gmail.com>
2022-07-26 21:00:45 -0300

    Swagger/springdocs

--- src/main/java/br/com/tt/petfeliz/controller/InformacoesController.java
+++ src/main/java/br/com/tt/petfeliz/controller/InformacoesController.java
@@ -1,11 +1,18 @@
 package br.com.tt.petfeliz.controller;
 
 import br.com.tt.petfeliz.service.InformacoesService;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@OpenAPIDefinition(info = @Info(title = "PET FELIZ API"))
+@Tag(name = "Informações")
 @RestController // Por padrão Singleton - 1 instância criada no startup
 //@SessionScope // Muda de Singleton para um Bean que nasce e morre a cada usuário
 public class InformacoesController {
@@ -16,17 +23,18 @@ public class InformacoesController {
          this.informacoesService = informacoesService;
     }
 
-    @RequestMapping("/telefones")
+    @GetMapping("/telefones")
+    @Operation(description = "Lista os telefones ativos do PetFeliz")
     public List<String> listarTelefones(){
         return informacoesService.listarTelefones();
     }
 
-    @RequestMapping("/enderecos")
+    @GetMapping("/enderecos")
     public List<String> listarEnderecos(){
         return informacoesService.listarEnderecos();
     }
 
-    @RequestMapping("/horarios")
+    @GetMapping("/horarios")
     public List<String> listarHorarios(){
         return informacoesService.listarHorarios();
     }
